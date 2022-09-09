44b2a00d86495c19b2b7943e454f36b2cfc32d80
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-09-09 17:26:37 +0530

    Updated

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def insertionSort(array) -> List[int]:
     while j >= 0 and key < array[j]:
       array[j + 1] = array[j]
       j = j - 1
-    array[j + 1] = key
+    # array[j + 1] = key
   return array
 
 # data = [9, 5, 1, 4, 3]
